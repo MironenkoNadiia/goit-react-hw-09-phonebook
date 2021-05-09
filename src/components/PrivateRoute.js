61348f4888a10a5d932c2c9353c3744edb2eec83
MironenkoNadiia <candleflame@i.ua>
2021-05-09 21:45:41 +0300

    privateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { connect } from "react-redux";
 import authSelectors from "../redux/Auth/auth-selectors";
 
-export default function PrivateRoute({ redirectTo, children, ...routeProps }) {
-  const isAuthenticated = useSelector(authSelectors.getIsLogedIn);
-  return (
-    <Route {...routeProps}>
-      {isAuthenticated ? children : <Redirect to={redirectTo} />}
-    </Route>
-  );
-}
+const PrivateRoute = ({
+  component: Component,
+  isAuthenticated,
+  redirectTo,
+  ...routeProps
+}) => (
+  <Route
+    {...routeProps}
+    render={(props) =>
+      isAuthenticated ? <Component {...props} /> : <Redirect to={redirectTo} />
+    }
+  />
+);
 
-// const mapStateToProps = (state) => ({
-//   isAuthenticated: authSelectors.getIsLogedIn(state),
-// });
+const mapStateToProps = (state) => ({
+  isAuthenticated: authSelectors.getIsLogedIn(state),
+});
 
-// export default connect(mapStateToProps)(PrivateRoute);
+export default connect(mapStateToProps)(PrivateRoute);
